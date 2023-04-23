6b351441d5e001f126a144412a0ba7dfb744f0b9
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-04-23 14:39:33 +0100

    helper functions

---
+++ helper_functions.c
@@ -0,0 +1,16 @@
+
+/**
+* char_len - calc chars lenght
+* @c: character or string
+* Return: len
+*/
+
+
+int char_len(char *c)
+{
+	int i = 0;
+
+	while (*(c + i))
+		i++;
+	return (i);
+}
--- main.h
+++ main.h
@@ -3,13 +3,15 @@
 
 #include <stdarg.h>
 #include <unistd.h>
+
 /**
  * option - Stucture for Option
  * @flag: String
  * @fun: Pointer to function.
  *
- * Descripotion: Define a new type struct opt whit flag and fun.
+ * Description: Define a new type struct opt whit flag and fun.
  */
+
 typedef struct option
 {
 	char *flag;
@@ -17,7 +19,10 @@ typedef struct option
 
 } opt;
 
-/* Prototypes */
+/*functions Prototypes */
 int _printf(const char *format, ...);
 
+
+/*helper functions prototypes */
+int char_len(char *c);
 #endif
