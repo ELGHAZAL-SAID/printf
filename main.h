bf8f93106c0a84f64e55c4dd2b3c69c5cb073bef
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-23 06:21:47 -0700

    Header File

--- main.h
+++ main.h
@@ -1,6 +1,18 @@
 #ifndef MAIN_H
-#define MAIN_H
+#define MAIN_
+/**
+ * option - Stucture for Option
+ * @flag: String
+ * @fun: Pointer to function.
+ *
+ * Descripotion: Define a new type struct opt whit flag and fun.
+ */
+typedef struct option
+{
+	char *flag;
+	void (*fun)();
 
-
+} opt;
+int _printf(const char *format, ...);
 
 #endif
