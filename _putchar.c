62c679ac303edb7b0fe138a5554b9be5ec7b2ca0
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-23 13:31:07 -0700

    Betty style for file _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,13 +1,11 @@
 #include "main.h"
-
-
 /**
-*
-*
-*
-*/
-
+ *_putchar - Function printing character
+ *@buffersize: String
+ *@buffer: String
+ *Return: Integer
+ */
 int _putchar(char *buffer, int buffersize)
 {
-	return(write(1, buffer, buffersize));
+	return (write(1, buffer, buffersize));
 }
