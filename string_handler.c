c0b96ac38e781ebfe5e8674fc97ee9243d5d90b3
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-23 16:18:57 -0700

    Update Files

--- string_handler.c
+++ string_handler.c
@@ -16,11 +16,14 @@ int string_handler(va_list arg, char *buffer, unsigned int buffer_size)
 	if (value == 0)
 	{
 		for (i = 0; null_value[i]; i++)
+		{
 			buffer_size = buffer_handler(buffer, null_value[i], buffer_size);
+		}
 		return (6);
 	}
 	for (i = 0; *(value + i); i++)
+	{
 		buffer_size = buffer_handler(buffer, value[i], buffer_size);
-
+	}
 	return (i);
 }
