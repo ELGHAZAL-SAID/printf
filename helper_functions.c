22ebc5432c3f95ab7b9fa189e1ace021ff14b81d
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-23 13:05:42 -0700

    Betty style for helper_functions.c

--- helper_functions.c
+++ helper_functions.c
@@ -1,11 +1,9 @@
 #include "main.h"
 /**
-* char_len - calc chars lenght
-* @c: character or string
-* Return: len
-*/
-
-
+ * char_len - calc chars lenght
+ * @c: character or string
+ * Return: lenght of string
+ */
 int char_len(char *c)
 {
 	int i = 0;
