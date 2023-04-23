bd91c373f43e674fe100554f0169024a58b0b039
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-04-23 20:39:38 +0100

    handling functions errors

--- printf_handler.c
+++ printf_handler.c
@@ -35,12 +35,6 @@ int (*print_handler(const char *sp, int idx))(va_list, char *, unsigned int)
 }
 
 
-/**
- * ev_print_func - returns the amount of identifiers.
- * @s: argument indentifier
- * @index: index of argument identifier.
- * Return: amount of identifiers.
- */
 int ev_print_func(const char *s, int index)
 {
 	
