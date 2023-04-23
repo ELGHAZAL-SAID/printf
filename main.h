3e5cfbb9d5a941bce88c60dfb75f7ba8f8ee3392
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-04-23 21:41:31 +0100

    edit header file

--- main.h
+++ main.h
@@ -21,6 +21,7 @@ typedef struct option
 
 } opt;
 
+
 /*functions Prototypes */
 int (*print_handler(const char *sp, int idx))(va_list, char *, unsigned int);
 int _printf(const char *format, ...);
@@ -28,11 +29,15 @@ int char_handler(va_list arg, char *buffer, unsigned int buffer_size);
 int di_handler(va_list arg, char *buffer, unsigned int buffer_size);
 int string_handler(va_list arg, char *buffer, unsigned int buffer_size);
 
+
 /*buffer handlers*/
 unsigned int buffer_handler(char *buffer, char ch, unsigned int buffer_size);
 int _putchar(char *buffer, int buffersize);
 int ev_print_func(const char *s, int index);
 
+
 /*helper functions prototypes */
 int char_len(char *c);
+
+
 #endif
