6b8b3aee60125703844988f27e2a237a74d9e541
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-25 16:24:25 -0700

    handle whit Address %p

--- address_handler.c
+++ address_handler.c
@@ -1,13 +1,11 @@
 #include "main.h"
-
 /**
-*
-*
-*
-*
-*
-*/
-
+ * address_handler - handle address
+ * @arg: argument de type va_list
+ * @buffer: String
+ * @buffer_size: INteger
+ * Return: len of add
+ */
 int address_handler(va_list arg, char *buffer, unsigned int buffer_size)
 {
 	void *address;
@@ -17,42 +15,31 @@ int address_handler(va_list arg, char *buffer, unsigned int buffer_size)
 	char null_value[] = "(nil)";
 
 	address = va_arg(arg, void *);
-
 	if (address == 0)
 	{
-		while(null_value[i])
-		{
-			buffer_size = buffer_handler(buffer, null_value[i], buffer_size);
-			i++;
-		}
+		while (null_value[i])
+			buffer_size = buffer_handler(buffer, null_value[i], buffer_size), i++;
 		return (buffer_size);
 	}
-
 	input = (intptr_t)address;
-
 	if (input < 0)
 	{
 		input = (input * -1) - 1;
 		negative = 1;
 	}
-
 	binary = malloc(sizeof(char) * (64 + 1));
 	binary = to_binary(binary, input, negative, 64);
 	hexa = malloc(sizeof(char) * (16 + 1));
 	hexa = to_hexadecimal(binary, hexa, 0, 16);
 	buffer_size = buffer_handler(buffer, 48, buffer_size);
 	buffer_size = buffer_handler(buffer, 120, buffer_size);
-
 	counter = 2;
-	while(hexa[i])
+	while (hexa[i])
 	{
 		if (hexa[i] != '0' && !first_address)
 			first_address = 1;
 		if (first_address == 1)
-		{
-			buffer_size = buffer_handler(buffer, hexa[i], buffer_size);
-			counter++;
-		}
+			buffer_size = buffer_handler(buffer, hexa[i], buffer_size), counter++;
 		i++;
 	}
 	return (counter);
--- testing/main.c
+++ testing/main.c
@@ -33,6 +33,7 @@ int main(void)
     printf("Character:[%c]\n", 'H');
     _printf("String:[%s]\n", "I am a string !");
     printf("String:[%s]\n", "I am a string !");
+    _printf("\n%S\n", "best\nSchool");
     _printf("Address:[%p]\n", addr);
     printf("Address:[%p]\n", addr);
     len = _printf("Percent:[%%]\n");
